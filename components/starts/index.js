5d91f7ec21ff052754ed1878f61ac3ee404016a7
leofe <leofe200@163.com>
2018-08-30 02:27:42 +0800

    更新

---
+++ components/starts/index.js
@@ -0,0 +1,26 @@
+// components/starts/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    score: {
+      type: Number,
+      value: 0
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
---
+++ pages/hot-movie/index.js
@@ -0,0 +1,94 @@
+// pages/hot-movie/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    movieList: [{
+        title: '测试',
+        score: 8.7,
+        coverageUrl: 'https://img3.doubanio.com/view/photo/s_ratio_poster/public/p480747492.jpg'
+      },
+      {
+        title: '测试',
+        score: 8.7,
+        coverageUrl: 'https://img3.doubanio.com/view/photo/s_ratio_poster/public/p480747492.jpg'
+      },
+      {
+        title: '测试',
+        score: 8.7,
+        coverageUrl: 'https://img3.doubanio.com/view/photo/s_ratio_poster/public/p480747492.jpg'
+      },
+      {
+        title: '测试',
+        score: 8.7,
+        coverageUrl: 'https://img3.doubanio.com/view/photo/s_ratio_poster/public/p480747492.jpg'
+      },
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+  // 监听搜索
+  onShearchMovie: function (event) {
+    console.log('search')
+    let {value} = event.detail
+    wx.showToast({
+      title: value
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
